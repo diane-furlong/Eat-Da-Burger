209275bccc39e8727d13aec20b09480350220336
Diane Furlong <dfurlongm@gmail.com>
2021-02-24 23:42:07 -0500

    made all of the files with the prelim code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Molly');
+INSERT INTO burgers (name) VALUES ('Charlie');
+INSERT INTO burgers (name, devoured) VALUES ('Poppy', true);
+INSERT INTO burgers (name, devoured) VALUES ('Oscar', true);
+INSERT INTO burgers (name, devoured) VALUES ('Smudge', true);
+INSERT INTO burgers (name) VALUES ('Daisy');
