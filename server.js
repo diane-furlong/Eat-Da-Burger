0501753cd510b45d0d3cff2c80caaf335e9a10c6
Diane Furlong <dfurlongm@gmail.com>
2021-03-25 19:42:04 -0400

    compression installed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var compression = require('compression')
 
 const PORT = process.env.PORT || 8080;
 
@@ -10,6 +11,18 @@ app.use(express.static('public'));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
 
 // Set Handlebars.
 const exphbs = require('express-handlebars');
